38c3d6453c58a90f6e2559cbdea7d7b2538bbd3c
JefferGonzalez <jefferd2016@gmail.com>
2024-01-28 21:11:29 -0500

    feat: 🥳 Add controller to create slugs in free tier

--- src/controllers/slug.ts
+++ src/controllers/slug.ts
@@ -1,5 +1,7 @@
 import prisma from '@/db/client'
-import { notFound } from '@hapi/boom'
+import { type Slug } from '@/schemas/Slug'
+import { conflict, notFound } from '@hapi/boom'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 import { type NextFunction, type Request, type Response } from 'express'
 
 export const getSlug = async (
@@ -16,10 +18,77 @@ export const getSlug = async (
       }
     })
 
-    if (data === null) throw notFound('Not found')
+    const free = await prisma.slug.findUnique({
+      where: { slug },
+      select: {
+        url: true,
+        expired_at: true
+      }
+    })
 
-    return res.status(200).json({ data })
+    if (data === null && free === null) {
+      throw notFound('Not found')
+    }
+
+    if (data !== null) {
+      return res.status(200).json({ data })
+    }
+
+    if (free !== null) {
+      const expiredAt = free.expired_at
+      const date = new Date(Date.now())
+
+      if (date > expiredAt) {
+        await prisma.slug.delete({
+          where: { slug }
+        })
+
+        throw notFound('Not found')
+      }
+    }
+
+    return res.status(200).json({ data: free })
   } catch (error) {
+    next(error)
+  }
+}
+
+export const create = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response | void> => {
+  try {
+    const { slug, url }: Slug = req.body
+
+    const data = await prisma.slugs.findUnique({
+      where: { slug }
+    })
+
+    if (data !== null) {
+      throw conflict('Slug already exists')
+    }
+
+    const date = new Date(Date.now() + 3600000)
+
+    const newSlug = await prisma.slug.create({
+      data: {
+        slug,
+        url,
+        expired_at: date
+      },
+      select: {
+        id: true
+      }
+    })
+
+    return res.status(201).json({ data: newSlug })
+  } catch (error) {
+    if (error instanceof PrismaClientKnownRequestError) {
+      if (error.code === 'P2002') {
+        next(conflict('Slug already exists'))
+      }
+    }
     next(error)
   }
 }
--- src/routes/slug.ts
+++ src/routes/slug.ts
@@ -1,8 +1,12 @@
-import { getSlug } from '@/controllers/slug'
+import { create, getSlug } from '@/controllers/slug'
+import { validateSchema } from '@/middlewares/validator'
+import { SlugSchema } from '@/schemas/Slug'
 import { Router } from 'express'
 
 const router = Router()
 
-router.get('/slug/:slug', getSlug)
+router
+  .get('/slug/:slug', getSlug)
+  .post('/slug', validateSchema(SlugSchema, 'body'), create) // free tier
 
 export default router
