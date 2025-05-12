432fcf189e0dc7ca0f5c24016b78abceac19d998
JefferGonzalez <jefferd2016@gmail.com>
2025-05-12 12:36:00 -0500

    chore: ⚙️ Migrate from ts-node-dev to tsx and update Vercel dAPI folder

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 import app from '@/app.js'
 import config from '@/config.js'
 
-const init = (): void => {
-  const port = app.get('port') as string
+const port = app.get('port') as string
 
-  app.listen(port, () => {
-    if (config.ENV === 'development') {
-      console.clear()
-      console.log(
-        '🚀 \x1b[37m\x1b[1m%s\x1b[0m \x1b[34m%s\x1b[0m',
-        'Server is running in:',
-        `http://localhost:${port}`
-      )
-    }
-  })
-}
+app.listen(port, () => {
+  if (config.ENV === 'development') {
+    console.clear()
+    console.log(
+      '🚀 \x1b[37m\x1b[1m%s\x1b[0m \x1b[34m%s\x1b[0m',
+      'Server is running in:',
+      `http://localhost:${port}`
+    )
+  }
+})
 
-init()
+export default app
